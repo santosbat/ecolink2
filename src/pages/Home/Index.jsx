666ada7d49ee2111af052c43e8af75b7b8657640
ljuliol <jcjulio.contato@gmail.com>
2024-10-03 00:44:08 -0300

    imagens e icones

--- src/pages/Home/Index.jsx
+++ src/pages/Home/Index.jsx
@@ -1,9 +1,19 @@
+import { Link } from "react-router-dom"
+
 function Home() {
   return(
 
-    <h2>Home</h2>
+    <div className="container">
 
-)
+  < h2>Home</h2>
+
+
+  <Link to={"/Cadastro"}> Realize seu cadastro</Link>
+
+    </div>
+    
+
+  )
 }
 
 export default Home
